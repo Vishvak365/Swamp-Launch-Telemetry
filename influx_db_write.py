bbe286397cc3cfe0cd2268db0bf683fc50b531b6
Vishvak Seenichamy <vishvak365@gmail.com>
2021-09-01 22:53:16 -0400

    Add base influxDB connection

---
+++ influx_db_write.py
@@ -0,0 +1,26 @@
+import influxdb_client
+import time
+import random
+from influxdb_client.client.write_api import SYNCHRONOUS
+bucket = "vishvak1"
+org = "vishvak1"
+# Token not a secret - only access on local
+token = "nyEO6OM0nUBpGdZsJtneUGlEtO2DQknvb7yt9fWBMoa2cuAcqmOIL_nYcE6n6t1IqlL1HeOCgNMRbDVNU-ptVg=="
+# Store the URL of your InfluxDB instance
+url = "http://localhost:8086"
+
+client = influxdb_client.InfluxDBClient(
+    url=url,
+    token=token,
+    org=org
+)
+
+write_api = client.write_api()
+while True:
+    temp = float(random.randint(0, 100))
+    p = influxdb_client.Point("my_measurement").tag(
+        "location", "Prague").field("temperature", temp).time(time.time_ns())
+        # .tag("timestamp", str(time.time_ns()))
+    write_api.write(bucket=bucket, org=org, record=p,write_precision='ns')
+    print("wrote temp", temp)
+    time.sleep(.2)
